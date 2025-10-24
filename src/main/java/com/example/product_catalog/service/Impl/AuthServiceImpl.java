2f82ed02f2b5d65c6f4d870a434bb7cbd2ac91ba
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-24 23:01:55 +0100

    Implement AuthServiceImpl for authentication logic

---
+++ src/main/java/com/example/product_catalog/service/Impl/AuthServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.product_catalog.service.Impl;
+
+import com.example.product_catalog.payload.dto.LoginRequest;
+import com.example.product_catalog.payload.dto.RegisterRequest;
+import com.example.product_catalog.payload.response.AuthResponse;
+import com.example.product_catalog.service.AuthService;
+
+public class AuthServiceImpl implements AuthService {
+
+    @Override
+    public AuthResponse login(LoginRequest request) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'login'");
+    }
+
+    @Override
+    public AuthResponse register(RegisterRequest request) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'register'");
+    }
+
+}
