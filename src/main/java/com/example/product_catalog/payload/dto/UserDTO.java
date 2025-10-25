7d328deed0fe378f72d3902f35e7cbfcaa663d68
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-25 01:11:00 +0100

    Update UserDTO

--- src/main/java/com/example/product_catalog/payload/dto/UserDTO.java
+++ src/main/java/com/example/product_catalog/payload/dto/UserDTO.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import com.example.product_catalog.entity.Role;
 
+import lombok.Builder;
+import lombok.Data;
 
 
+@Data
+@Builder
 public class UserDTO {
 
     private String userName;
