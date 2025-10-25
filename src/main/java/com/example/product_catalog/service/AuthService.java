6990ded866adba18d57cc836c7bd98ee91be8581
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-25 01:11:00 +0100

    Update AuthService interface

--- src/main/java/com/example/product_catalog/service/AuthService.java
+++ src/main/java/com/example/product_catalog/service/AuthService.java
@@ -1,8 +1,13 @@
 package com.example.product_catalog.service;
 
-
-
+import com.example.product_catalog.payload.dto.LoginRequest;
+import com.example.product_catalog.payload.dto.RegisterRequest;
+import com.example.product_catalog.payload.response.AuthResponse;
 
 public interface AuthService {
+
+    AuthResponse login(LoginRequest request) throws Exception;
+    
+    AuthResponse register(RegisterRequest request) throws Exception;
 
 }
