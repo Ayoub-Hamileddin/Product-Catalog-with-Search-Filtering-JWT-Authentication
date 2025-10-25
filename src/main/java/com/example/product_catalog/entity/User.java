1b3ff29c4076af5ef7f72aca0ae28789c595f2cf
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-25 01:10:59 +0100

    Update User entity

--- src/main/java/com/example/product_catalog/entity/User.java
+++ src/main/java/com/example/product_catalog/entity/User.java
@@ -7,11 +7,15 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,18 +25,22 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User implements UserDetails {
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String userName;
 
+   @Column(unique = true,nullable = false) 
+   @Email
     private String email;
 
     private String password;
 
     @ManyToMany
-    private Role role;
+    private List<Role> roles;
 
 
 
@@ -40,7 +48,9 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-       return List.of(new SimpleGrantedAuthority(role.getName().name()));
+       return roles.stream()
+                  .map(role->new SimpleGrantedAuthority(role.getName().name()))
+                  .toList();
     }
 
     @Override
