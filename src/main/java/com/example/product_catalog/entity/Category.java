e7d415e4ff0102c346ed7591df8edf44a8b8a016
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-30 01:53:21 +0100

    Add Category entity with OneToMany relation to Product

---
+++ src/main/java/com/example/product_catalog/entity/Category.java
@@ -0,0 +1,31 @@
+package com.example.product_catalog.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+ private Long id ;
+ private String name ;
+ @OneToMany
+ private List<Product> product;
+
+
+}
