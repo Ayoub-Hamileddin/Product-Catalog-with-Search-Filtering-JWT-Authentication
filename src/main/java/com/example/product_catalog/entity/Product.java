ec2216244e3e5bf0978c683479f40d05689b8b68
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-10-30 01:53:21 +0100

    Add Product entity with ManyToOne relation to Category

---
+++ src/main/java/com/example/product_catalog/entity/Product.java
@@ -0,0 +1,33 @@
+package com.example.product_catalog.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id ;
+    private String name ;
+    private String description ;
+    private Double price ;
+    private Double rating ;
+    private Boolean inStock ;
+    @ManyToOne
+    private Category category ;
+
+
+}
