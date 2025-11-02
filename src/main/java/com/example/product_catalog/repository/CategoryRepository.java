6dd43910faa45882681730e21372d970ed1411f0
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-02 03:10:55 +0100

    Update CategoryRepository

--- src/main/java/com/example/product_catalog/repository/CategoryRepository.java
+++ src/main/java/com/example/product_catalog/repository/CategoryRepository.java
@@ -1,8 +1,9 @@
 package com.example.product_catalog.repository;
 
-import java.util.Locale.Category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.product_catalog.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category,Long> {
 
