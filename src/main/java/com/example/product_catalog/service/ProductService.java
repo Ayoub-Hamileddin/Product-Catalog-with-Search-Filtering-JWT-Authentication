cb863575adf0cf87ca98d6d9a0e7f69597197b82
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-10 03:59:38 +0100

    update: extended ProductService interface with filtering methods

--- src/main/java/com/example/product_catalog/service/ProductService.java
+++ src/main/java/com/example/product_catalog/service/ProductService.java
@@ -2,19 +2,18 @@ package com.example.product_catalog.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+
 import com.example.product_catalog.payload.dto.ProductDTO;
 
 public interface ProductService {
-    List<ProductDTO> getAllProduct( String name,
-                                   String description,
-                                   String categoryName,
-                                   Double minPrice,
-                                   Double maxPrice,
-                                   Boolean InStock,
-                                   String  orderBy,
-                                   String  sortBy);
+    List<ProductDTO> getAllProduct(Pageable  pageable , String search ,String name);
+
     ProductDTO getProductById(Long Id) throws Exception;
+
     ProductDTO createProduct(ProductDTO productDTO) throws Exception;
+
     ProductDTO updateProduct(Long ProductId,ProductDTO productDTO) throws Exception;
+
     void deleteProduct(Long Id) throws Exception;
 }
