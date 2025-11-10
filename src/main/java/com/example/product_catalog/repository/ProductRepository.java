623e4246f7e91494518ee6bd3aa3e86dd5f9d3ae
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-10 03:59:37 +0100

    update: improved repository logic for product filtering

--- src/main/java/com/example/product_catalog/repository/ProductRepository.java
+++ src/main/java/com/example/product_catalog/repository/ProductRepository.java
@@ -2,33 +2,17 @@ package com.example.product_catalog.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.example.product_catalog.entity.Product;
 
-public interface ProductRepository extends JpaRepository<Product,Long> {
+public interface ProductRepository extends JpaRepository<Product,Long>,JpaSpecificationExecutor<Product> {
 
-    @Query(
-        "SELECT p from Product p "+ 
-        "WHERE ("+
-        " LOWER(p.name) LIKE LOWER(CONCAT('%',:name,'%')) OR"+
-        " LOWER(p.description) LIKE LOWER(CONCAT('%',:description,'%'))) AND (  " +
-        " (p.category.id = :categoryName) OR "+
-        " (p.price >= :minPrice)  OR " +
-        " (p.price <= :maxPrice) OR " +
-        " (p.inStock = true) ) " +
-        " ORDER BY :orderBy ASC "+
-        " LIMIT 10 OFFSET 10 "
-    )
-    List<Product> findProducts(@Param("name") String name,
-                                  @Param("description") String description,
-                                  @Param("categoryName") String categoryName,
-                                  @Param("minPrice") Double minPrice,
-                                  @Param("maxPrice") Double maxPrice,
-                                  @Param("InStock") Boolean InStock,
-                                  @Param("orderBy") String  orderBy,
-                                  @Param("sortBy") String  sortBy
-                                  );
+        Page<Product> findByNameOrDescription(String description,String search,Pageable pageable);
+
+
+
 }
