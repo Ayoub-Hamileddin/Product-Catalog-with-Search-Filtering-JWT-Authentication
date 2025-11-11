6ba76f9d9429c65e9a10c55a5547882e816cc4e9
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-11-11 11:00:42 +0100

    Add advanced product filtering specifications (keyword, category, price range, availability, rating)

--- src/main/java/com/example/product_catalog/specification/ProductSpecification.java
+++ src/main/java/com/example/product_catalog/specification/ProductSpecification.java
@@ -1,28 +1,36 @@
 package com.example.product_catalog.specification;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 import org.springframework.data.jpa.domain.Specification;
 
 import com.example.product_catalog.entity.Category;
 import com.example.product_catalog.entity.Product;
 
 import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Predicate;
 
 
 public class ProductSpecification {
 
+
+         // * ----------------------- Searching--------------------
       public static Specification<Product> hasKeyword(String search){
             return (root, query, criteriaBuilder) ->{
                if(search==null || search.isEmpty()) return criteriaBuilder.conjunction();
-                  String like ="%" +search.toLowerCase()+ "%";
                      return criteriaBuilder.or(
-                        criteriaBuilder.like(root.get("name"), search),
-                        criteriaBuilder.like(root.get("description"), search)
+                        criteriaBuilder.like(root.get("name"), "%"+search+"%"),
+                        criteriaBuilder.like(root.get("description"), "%"+search+"%")
                      );
       };
          
-      }
+   }
 
+
+      // * ----------------------- filter by Category--------------------
       public static Specification<Product> withCategory(String name){
             return (root,query,cb)->{
               if(name==null) return cb.conjunction();
@@ -31,31 +39,52 @@ public class ProductSpecification {
             };
       }
 
-      // public static Specification<Product> rangePrice(Long id){
-      //       return (root,query,cb)->{
-      //         if(id==null) return cb.conjunction();
-      //          Join<Product,Category> joinCategory=root.join("category");
-      //          return cb.equal(joinCategory.get("id"), id);
-      //       };
-      // }
+
+
+         // * ----------------------- Price range filtering--------------------
+      public static Specification<Product> rangePrice(Double minPrice,Double maxPrice){
+            return (root,query,cb)->{
+               List<Predicate>predicates=new ArrayList<>();
+
+               if (minPrice !=null) {
+                     predicates.add(cb.greaterThanOrEqualTo(root.get("price"), minPrice));
+               }
+
+               if (maxPrice !=null) {
+                     predicates.add(cb.lessThanOrEqualTo(root.get("price"), maxPrice));
+               }
+
+               return cb.and(predicates.toArray(new Predicate[0]));
+
+            };
+      }
+
+
+      // * ----------------------- filter availability--------------------
+      public static Specification<Product> availibilty(Boolean inStock){
+         return (root,query,cb)->{
+            if(inStock ==null) return cb.conjunction();
+            return cb.equal(root.get("inStock"), inStock);
+
+         };
+      }
+
+
+
+
+      // * ----------------------- rating --------------------
+      public static Specification<Product> rating(Double rating){
+         return (root,query,cb)->{
+            if(rating ==null) return cb.conjunction();
+           Predicate predicate = cb.lessThanOrEqualTo(root.get("rating"), rating);
+           query.orderBy(cb.desc(root.get("rating")));
+
+            return predicate;
+         };
+      }
 
 
  
 }
 
 
-// return new Specification<Product>() {
-
-//         @Override
-//         public Predicate toPredicate(Root<Product> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-//            if (search==null || search.isEmpty()) {
-//                 criteriaBuilder.conjunction();
-//            }
-
-//            List<Predicate> list=new ArrayList<>();
-//            list.add(criteriaBuilder.equal(root.get("name"), search));
-//            list.add(criteriaBuilder.equal(root.get("description"), search));
-//            return null;
-//         }
-        
-//     };
